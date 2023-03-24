4480b6f1ccc33ba3458415e755e00d5359131413
Sourav Ganguly <souravganguly.bd@gmail.com>
2023-03-24 20:58:00 +0600

    Add colorfullness to the detail view

--- AI-API-Explorer/ContentView.swift
+++ AI-API-Explorer/ContentView.swift
@@ -9,47 +9,58 @@ import SwiftUI
 
 struct ContentView: View {
     let items = ["Item 1", "Item 2", "Item 3"]
-    
+
     var body: some View {
         NavigationView {
             List(items, id: \.self) { item in
                 NavigationLink(destination: DetailView(item: item)) {
                     VStack(alignment: .leading, spacing: 8) {
                         Text(item)
-                            .font(.system(size: 20, weight: .medium))
+                            .font(.headline)
                             .foregroundColor(.primary)
                         Text("Description of \(item)")
                             .font(.subheadline)
                             .foregroundColor(.secondary)
                     }
+                    .padding(.vertical, 8)
+                    .padding(.horizontal, 16)
                 }
-                .padding(.vertical, 8)
             }
             .navigationTitle("Items")
             .listStyle(.plain)
             .navigationBarTitleDisplayMode(.inline)
+            
         }
     }
 }
 
 struct DetailView: View {
     let item: String
-    
+
     var body: some View {
-        VStack(alignment: .leading, spacing: 8) {
-            Text(item)
-                .font(.system(size: 30, weight: .bold))
-                .foregroundColor(.primary)
-            Text("This is the detail view for \(item)")
-                .font(.body)
-                .foregroundColor(.secondary)
+        ZStack {
+            LinearGradient(
+                gradient: Gradient(colors: [.purple, .pink, .orange]),
+                startPoint: .topLeading,
+                endPoint: .bottomTrailing
+            )
+            .ignoresSafeArea()
+
+            VStack(alignment: .leading, spacing: 16) {
+                Text(item)
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .foregroundColor(.white)
+                Text("This is the detail view for \(item)")
+                    .font(.body)
+                    .foregroundColor(.white)
+            }
+            .padding()
         }
-        .padding()
         .navigationTitle(item)
         .navigationBarTitleDisplayMode(.inline)
     }
 }
-
 
 
 struct ContentView_Previews: PreviewProvider {
---
+++ AI-API-Explorer/DetailView.swift
@@ -0,0 +1,8 @@
+//
+//  DetailView.swift
+//  AI-API-Explorer
+//
+//  Created by Sourav on 24/3/23.
+//
+
+import Foundation
