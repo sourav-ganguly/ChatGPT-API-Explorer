7318f9dbeefc1db18d74d4bc6154c8b7afa76dcb
Sourav Ganguly <souravganguly.bd@gmail.com>
2023-03-24 23:36:48 +0600

    Create separate DetailView file

--- AI-API-Explorer/ContentView.swift
+++ AI-API-Explorer/ContentView.swift
@@ -32,35 +32,6 @@ struct ContentView: View {
     }
 }
 
-struct DetailView: View {
-    let item: String
-
-    var body: some View {
-        ZStack {
-            LinearGradient(
-                gradient: Gradient(colors: [.purple, .pink, .orange]),
-                startPoint: .topLeading,
-                endPoint: .bottomTrailing
-            )
-            .ignoresSafeArea()
-
-            VStack(alignment: .leading, spacing: 16) {
-                Text(item)
-                    .font(.largeTitle)
-                    .fontWeight(.bold)
-                    .foregroundColor(.white)
-                Text("This is the detail view for \(item)")
-                    .font(.body)
-                    .foregroundColor(.white)
-            }
-            .padding()
-        }
-        .navigationTitle(item)
-        .navigationBarTitleDisplayMode(.inline)
-    }
-}
-
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
--- AI-API-Explorer/DetailView.swift
+++ AI-API-Explorer/DetailView.swift
@@ -5,4 +5,32 @@
 //  Created by Sourav on 24/3/23.
 //
 
-import Foundation
+import SwiftUI
+
+struct DetailView: View {
+    let item: String
+
+    var body: some View {
+        ZStack {
+            LinearGradient(
+                gradient: Gradient(colors: [.purple, .pink, .orange]),
+                startPoint: .topLeading,
+                endPoint: .bottomTrailing
+            )
+            .ignoresSafeArea()
+
+            VStack(alignment: .leading, spacing: 16) {
+                Text(item)
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .foregroundColor(.white)
+                Text("This is the detail view for \(item)")
+                    .font(.body)
+                    .foregroundColor(.white)
+            }
+            .padding()
+        }
+        .navigationTitle(item)
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
